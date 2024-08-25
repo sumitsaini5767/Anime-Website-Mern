f36e84734f1ab9bc6aef63c238f10febb377d554
sumit <sumitsaini576@gmail.com>
2024-08-25 20:48:10 +0530

    comment added for the useloaction hook

--- src/pages/CommonPage/Common.jsx
+++ src/pages/CommonPage/Common.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 
 const Common = () => {
+  //useLocation is a hook from React Router that returns the location object, which represents the current URL in the app.
   const location = useLocation();
   const { PageType } = location.state || {}; // Guarding against location.state being null
 
